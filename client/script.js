2846473e8ab79dec17a9cffdac837f83d1502271
duyminh1965 <buysvi@gmail.com>
2022-12-26 09:58:46 +0700

    update the server url

--- client/script.js
+++ client/script.js
@@ -76,9 +76,10 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
 
   loader(messageDiv);
+
   // fetch data from server -> bot's response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codexopen.onrender.com', {
     method: 'POST',
     headers:{
       'Content-Type': 'application/json'
